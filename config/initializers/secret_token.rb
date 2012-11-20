8aa8e24c283f9ed49062705588a748a08a6fdb4e
Andy Holman <andy@conspyre.com>
2012-11-19 20:12:54 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phoenix::Application.config.secret_token = '24a2da31f9c755b2017c938c50582c2cb8ee08200fbdcb72564d55986864f9c936dc79eb58059ace4a88f3e27cbfc07b414fc8fed5ffe363f54f18549f9459c5'
