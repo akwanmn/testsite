52912b7a26c561b19f52791fd1d0348a1a0c3923
Andy Holman <andy@conspyre.com>
2013-01-02 21:02:39 -0500

    Recalculate profile percentage on image upload.

--- app/assets/javascripts/admin/user_profile.js.coffee
+++ app/assets/javascripts/admin/user_profile.js.coffee
@@ -12,7 +12,7 @@ $ ->
       file = data.files[0]
       if types.test(file.type) || types.test(file.name)
         data.context = $(tmpl("template-upload", file))
-        $('#new_painting').append(data.context)
+        $('#photos').prepend(data.context)
         data.submit()
       else
         alert("#{file.name} is not a gif, jpeg, or png image file")
--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -8,7 +8,10 @@ class Admin::PhotosController < ApplicationController
     user = User.find(params[:user_id])
     @photo = user.photos.new(params[:photos])
     respond_to do |format|
-      @photo.save!
+      if @photo.save!
+        # do this to just update their profile percentage.
+        user.user_profile.update_attribute(:percent_complete, user.user_profile.calculate_profile_percentage)
+      end
       format.js { render layout: false }
     end
   end
