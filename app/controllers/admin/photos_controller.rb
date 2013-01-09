5b651481e8e1b46f54ef81da82e927dfc1b2abad
Andy Holman <andy@conspyre.com>
2013-01-08 20:55:46 -0500

    Fix the delete image links.

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -5,20 +5,20 @@ class Admin::PhotosController < ApplicationController
   end
 
   def create
-    user = User.find(params[:user_id])
-    @photo = user.photos.new(params[:photos])
+    @user = User.find(params[:user_id])
+    @photo = @user.photos.new(params[:photos])
     respond_to do |format|
       if @photo.save!
         # do this to just update their profile percentage.
-        user.user_profile.update_attribute(:percent_complete, user.user_profile.calculate_profile_percentage)
+        @user.user_profile.update_attribute(:percent_complete, @user.user_profile.calculate_profile_percentage)
       end
       format.js { render layout: false }
     end
   end
 
   def destroy
-    user = User.find(params[:user_id])
-    @photo = user.photos.find(params[:id])
+    @user = User.find(params[:user_id])
+    @photo = @user.photos.find(params[:id])
     pass = true
     respond_to do |format|
       if @photo.destroy
