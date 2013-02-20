c1e68de5e972e469d5f43741b9eea8fddf000e45
Andy Holman <andy@conspyre.com>
2013-02-19 20:24:47 -0500

    add seeking to query so we only show what they are looking for

--- app/controllers/lounge/dashboard_controller.rb
+++ app/controllers/lounge/dashboard_controller.rb
@@ -4,8 +4,8 @@ class Lounge::DashboardController < ApplicationController
     # now - max_age.years and now - min_age.years
     @users = User.near(current_user.address, current_user.user_profile.search_radius).
       where('user_profile.likes' => {'$in' =>  current_user.user_profile.likes}).and(
-        'user_profile.birthday' => {'$gte' => (Date.today - current_user.user_profile.max_age.years)}).and(
+        'user_profile.birthday' => {'$gte' => (Date.today.beginning_of_year - current_user.user_profile.max_age.years)}).and(
         'user_profile.birthday' => {'$lte' => (Date.today - current_user.user_profile.min_age.years)}
-      )
+      ).and('user_profile.gender' => current_user.user_profile.seeking)
   end
 end
