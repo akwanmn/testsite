b272f521ebdb083975748ec943c8a06dadc897b0
Andy Holman <andy@conspyre.com>
2013-05-27 11:42:33 -0400

    add some static pages

--- app/controllers/lounge/static_controller.rb
+++ app/controllers/lounge/static_controller.rb
@@ -1,27 +1,22 @@
 class Lounge::StaticController < ApplicationController
   skip_before_filter :authenticate_user!
+  layout 'layouts/static'
 
   def faq
-    render text: 'FAQ'
   end
 
   def terms
-    render text: 'Terms'
   end
 
   def tips
-    render text: 'Tips'
   end
 
   def articles
-    render text: 'Articles'
   end
 
   def success
-    render text: 'Success'
   end
 
   def privacy
-    render text: 'Privacy'
   end
 end
