26da7c208aee0d4ba05f3ac5d45d5b98b4c345a0
Andy Holman <ajholman@ctmtg.com>
2013-09-20 19:00:48 -0400

    Clean up photos controller

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -7,11 +7,8 @@ class Admin::PhotosController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @photo = @user.photos.new(params[:photos])
+    @user.user_profile.update_attribute(:percent_complete, @user.user_profile.calculate_profile_percentage) if @photo.save!
     respond_to do |format|
-      if @photo.save!
-        # do this to just update their profile percentage.
-        @user.user_profile.update_attribute(:percent_complete, @user.user_profile.calculate_profile_percentage)
-      end
       format.js { render layout: false }
     end
   end
