0173e5dff0fba7ee70f596be218ffe1259e1daea
Andy Holman <andy@conspyre.com>
2013-10-12 14:43:37 -0400

    Change up the show profile page, needs some more CSS love, but its getting there

--- app/helpers/lounge/profile_helper.rb
+++ app/helpers/lounge/profile_helper.rb
@@ -14,4 +14,53 @@ module Lounge::ProfileHelper
     options.select {|k| k[1].to_i % step == 1 }
   end
 
+  def marital_status(user_profile)
+    find_option MARITAL_STATUS, user_profile.marital_status
+  end
+
+  def fitness_status(user_profile)
+    find_option HEALTH_FITNESS, user_profile.health_fitness
+  end
+
+  def children(user_profile)
+    find_option CHILDREN, user_profile.children
+  end
+
+  def reading(user_profile)
+    find_option BOOKS_READING, user_profile.reading
+  end
+
+  def nightlife(user_profile)
+    find_option NIGHTLIFE, user_profile.nightlife
+  end
+
+  def drinking(user_profile)
+    find_option DRINKING, user_profile.drinking
+  end
+
+  def travel(user_profile)
+    find_option TRAVEL, user_profile.travel
+  end
+
+  def career(user_profile)
+    find_option CAREER, user_profile.career
+  end
+
+  def smoking(user_profile)
+    find_option SMOKING, user_profile.smoking
+  end
+
+  def eating(user_profile)
+    find_option EATING, user_profile.eating
+  end
+
+  def politics(user_profile)
+    find_option POLITICS, user_profile.politics
+  end
+
+  # look up an option from a constant.
+  def find_option(constant, value)
+    constant.select {|l| l[1] == value.to_s }[0][0]
+  end
+
 end
