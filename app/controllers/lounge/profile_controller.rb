9f386b5074a96f77e1001f5a12e612fa2c52e6f8
Andy Holman <ajholman@ctmtg.com>
2013-12-07 10:04:04 -0500

    Fix for saving on update match criteria

--- app/controllers/lounge/profile_controller.rb
+++ app/controllers/lounge/profile_controller.rb
@@ -10,7 +10,11 @@ class Lounge::ProfileController < ApplicationController
     @profile = @user.user_profile
     @profile.skip_validation = true
     @profile.likes = params[:likes]
-    @profile.update(params[:user_profile])
+    puts "*" * 50
+    p params
+    @profile.attributes = params[:user_profile]
+    p @profile
+    p @profile.save!
     redirect_to lounge_dashboard_index_path
   end
 
